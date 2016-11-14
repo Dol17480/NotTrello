36cc1e047d4ac7520b69e9a9faa994b51f265315
Dol174 <david.oleary174@gmail.com>
2016-11-14 10:25:30 +0000

    Fragments completed - AUF

--- app/src/main/java/com/example/user/nottrello/TaskFragment.java
+++ app/src/main/java/com/example/user/nottrello/TaskFragment.java
@@ -2,6 +2,12 @@ package com.example.user.nottrello;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by user on 14/11/2016.
@@ -10,12 +16,41 @@ import android.support.v4.app.Fragment;
 public class TaskFragment extends Fragment {
 
     private Task mTask;
+    private EditText mTitleField;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mTask = new Task();
-        
+
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_task, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.task_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(
+                    CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(
+                    CharSequence s, int start, int before, int count) {
+                    mTask.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
+    }
+
+
 }
